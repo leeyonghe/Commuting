41565425ef8dab09d7a07f03279cfa7af9811de9
lyhdra99 <lyhdra99@gmail.com>
2023-05-08 00:45:15 +0900

    Application 클래스 상속후 AOSApplication생성 및 적용

---
+++ app/src/main/java/com/aos_kotlin/AOSApplication.kt
@@ -0,0 +1,15 @@
+package com.aos_kotlin
+
+import android.app.Application
+
+class AOSApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun onTerminate() {
+        super.onTerminate()
+    }
+
+}
